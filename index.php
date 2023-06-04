90c594ed8114d6f0c5069f10b94235c03b605ff4
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-06-04 21:09:47 +0300

    ternary

--- index.php
+++ index.php
@@ -8,3 +8,13 @@
     print (1 <=> 2);
     print (2 <=> 2);
     print (3 <=> 2);
+
+    $output = `ls`;
+
+    print "<pre>$output</pre>";
+
+    $id = 10;
+
+    ($id < 19) ? print "Cool php" : print "Fake code";
+
+    
