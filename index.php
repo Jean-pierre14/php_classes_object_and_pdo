645c33ec3d07301f651a83ecdaab14aee1fadb5e
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-08-24 10:24:57 +0200

    add: nav and subnav

--- index.php
+++ index.php
@@ -1,20 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-    $firstName = "Grace";
-    $lastName = "Bisimwa";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-    echo ($firstName ?? "Unknown"). " " . ($lastName ?? "");
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name" placeholder="Name" class="form-co">
+                    </div>
+                    <div class="form-group">
+                        <label for="tags">Tags</label>
+                        <input type="text" name="tags" id="tags" placeholder="Tags: Orthographe, Vocabulaire, Grammaire"
+                            class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-8">
+                <h3>Data</h3>
+            </div>
+        </div>
+    </div>
+</body>
 
-    print (1 <=> 2);
-    print (2 <=> 2);
-    print (3 <=> 2);
-
-    $output = `ls`;
-
-    print "<pre>$output</pre>";
-
-    $id = 10;
-
-    ($id < 19) ? print "Cool php" : print "Fake code";
-
-    
+</html>
